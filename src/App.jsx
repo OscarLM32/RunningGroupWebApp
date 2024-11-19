48e005280b73ccfa912b08ab6b3f38bfea753cfd
Óscar López <oscar_lm_geminis@hotmail.es>
2024-11-19 12:04:00 +0100

    bootstrap navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/navbar/navbar"
 import Main from "./components/Main/main"
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./app.css"
 
 export default function App (){    
