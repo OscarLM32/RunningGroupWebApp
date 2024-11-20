9fbffe5ce981949e54f72b09e6d8183b9f37771c
Óscar López <oscar_lm_geminis@hotmail.es>
2024-11-20 23:02:43 +0100

    Home page styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,14 +2,16 @@
 
 function Hero(){
     return (
-        <section class="py-5 text-center container">
+        <section class="py-5 text-center container-fluid bg-dark text-light">
             <div class="row py-lg-5">
                 <div class="col-lg-6 col-md-8 mx-auto">
-                    <h1 class="fw-light">Album example</h1>
-                    <p class="lead text-body-secondary">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+                    <h1 class="fw-light">Welcome to RunTogether</h1>
+                    <p class="lead text-secondary">
+                        Discover a vibrant community of runners who share your passion. Whether you're a seasoned marathoner or just starting your journey, connect with groups near you, join events, and achieve your goals together.
+                    </p>
                     <p>
-                        <a href="#" class="btn btn-primary my-2">Main call to action</a>
-                        <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+                        <a href="/clubs" class="btn btn-primary my-2">Find a Running Club</a>
+                        <a href="/events" class="btn btn-outline-light my-2">Explore Upcoming Events</a>
                     </p>
                 </div>
             </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,5 @@
 import Album from "../../components/Album/Album"
 import Hero from "../../components/Hero/Hero"
-import "./Home.css"
 
 export default function Home(){
     return(
