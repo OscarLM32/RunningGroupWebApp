cdb31ae1b5184c3d49083811ea46999468e6c226
Óscar López <oscar_lm_geminis@hotmail.es>
2024-11-22 10:59:38 +0100

    class switched for className in Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,16 +2,16 @@
 
 function Hero(){
     return (
-        <div class="py-5 text-center stretch bg-dark text-light rounded-3 m-1">
-            <div class="row py-lg-5">
-                <div class="col-lg-6 col-md-8 mx-auto">
-                    <h1 class="fw-light">Welcome to RunTogether</h1>
-                    <p class="lead text-secondary">
+        <div className="py-5 text-center stretch bg-dark text-light rounded-3 m-1">
+            <div className="row py-lg-5">
+                <div className="col-lg-6 col-md-8 mx-auto">
+                    <h1 className="fw-light">Welcome to RunTogether</h1>
+                    <p className="lead text-secondary">
                         Discover a vibrant community of runners who share your passion. Whether you're a seasoned marathoner or just starting your journey, connect with groups near you, join events, and achieve your goals together.
                     </p>
                     <p>
-                        <a href="/clubs" class="btn btn-primary my-2">Find a Running Club</a>
-                        <a href="/events" class="btn btn-outline-light my-2">Explore Upcoming Events</a>
+                        <a href="/clubs" className="btn btn-primary my-2">Find a Running Club</a>
+                        <a href="/events" className="btn btn-outline-light my-2">Explore Upcoming Events</a>
                     </p>
                 </div>
             </div>
