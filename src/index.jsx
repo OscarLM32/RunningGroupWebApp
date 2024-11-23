f174193eadfc72ff333bc007b53ed0fdd9de9e95
Óscar López <oscar_lm_geminis@hotmail.es>
2024-11-23 20:50:18 +0100

    Included QueryClient to Index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { QueryClientProvider } from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 const root = createRoot(document.getElementById("root"))
 const queryClient = new QueryClient();
+
 root.render(
     <QueryClientProvider client={queryClient}>
            <App />
