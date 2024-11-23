6b33b8efddae176ff2420a916eab6383f577c3f4
Óscar López <oscar_lm_geminis@hotmail.es>
2024-11-23 20:51:00 +0100

    Renamed "class" to className in album component

--- src/components/Album/Album.jsx
+++ src/components/Album/Album.jsx
@@ -2,27 +2,27 @@
 
 function Album(){
     return (
-        <div class="album py-5 bg-dark text-light rounded m-1">
-            <div class="container">
+        <div className="album py-5 bg-dark text-light rounded m-1">
+            <div className="container">
 
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div class="col">
-                        <div class="card shadow-sm bg-secondary text-light">
-                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
+                <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    <div className="col">
+                        <div className="card shadow-sm bg-secondary text-light">
+                            <svg className="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false">
                                 <title>Placeholder</title>
                                 <rect width="100%" height="100%" fill="#6c757d"></rect>
                                 <text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
                             </svg>
-                            <div class="card-body">
-                                <p class="card-text">
+                            <div className="card-body">
+                                <p className="card-text">
                                     This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
                                 </p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <div class="btn-group">
-                                        <button type="button" class="btn btn-sm btn-outline-light">View</button>
-                                        <button type="button" class="btn btn-sm btn-outline-light">Edit</button>
+                                <div className="d-flex justify-content-between align-items-center">
+                                    <div className="btn-group">
+                                        <button type="button" className="btn btn-sm btn-outline-light">View</button>
+                                        <button type="button" className="btn btn-sm btn-outline-light">Edit</button>
                                     </div>
-                                    <small class="text-light">9 mins</small>
+                                    <small className="text-light">9 mins</small>
                                 </div>
                             </div>
                         </div>
