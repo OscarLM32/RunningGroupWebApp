21f11c5e2013c37adeb4681973ce47be7d6d4217
Óscar López <oscarlopezmaldonado32@gmail.com>
2024-12-04 01:18:01 +0100

    Changed axios config to https

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const api = axios.create({
-    baseURL: "http://localhost:5076/api",
+    baseURL: "https://localhost:7065/api",
     headers: {
         "Content-Type": "application/json"
     },
