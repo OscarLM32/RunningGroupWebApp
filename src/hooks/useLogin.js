cea5f91d8998433e7440f2301fae7cd49605c9d8
Óscar López <oscarlopezmaldonado32@gmail.com>
2024-12-04 12:23:37 +0100

    Implemented redirection to main page after

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,6 +1,7 @@
 import { useMutation } from "react-query";
 import { authenticationEndpoint } from "../services/endpoints"
 import api from "../services/api.js"
+import { useNavigate } from "react-router-dom";
 
 const loginEndpoint = authenticationEndpoint + "/login"
 
@@ -16,11 +17,13 @@ const loginRequest = async ({email, password}) => {
 
 
 export const useLogin = () => {
+    const navigate = useNavigate();
+    
     return useMutation({
         mutationFn: loginRequest,
         onSuccess: (data) => {
             console.log("Login successful:", data);
-            //redirect to dsahboard maybe
+            navigate("/");
         },
         onError: (error) => {
             console.log("Login failed", error); 
